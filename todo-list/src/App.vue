caccf6e8136d418e6f17ec7ea019690b68b4ec17
kharuk <kharuk.nastya@gmail.com>
2020-11-10 15:21:00 -0500

    add crud for todos

--- todo-list/src/App.vue
+++ todo-list/src/App.vue
@@ -1,26 +1,38 @@
 <template>
   <div id="app">
-    <v-text-field
-      label="What to do?"
-      v-modal="task"
-      :rules="rules"
-      :append-icon="mdi - map - marker"
-      @click:append="addTodo"
-    >
-      <v-icon>{{ icons.mdiAccount }}</v-icon>
-    </v-text-field>
-    <ul>
-      <li v-for="(item, index) in items" :key="item.id">
-        <h3>[#{{ index + 1 }}]</h3>
-        <p>Title: {{ item.title }}</p>
+    <div class="input__container">
+      <v-text-field
+        label="What to do?"
+        v-model="newTodo"
+        :rules="rules"
+        append-icon="mdi-plus"
+        @click:append="addTodo"
+      >
+      </v-text-field>
+       <p>Totally {{itemsCount}} items found.</p>
+    </div>
+    <ul class="todos__wrapper">
+      <li v-for="todo in filteredItems" :key="todo.id" class="todo__container">
+        <v-text-field
+          v-if="editableTodo === todo.id" 
+          v-model="newEditableTodo"
+          :rules="rules"
+        >
+        </v-text-field>
+        <p v-else>Title: {{ todo.title }}</p>
+         
+      
+        <div class="button__wrapper">
+        <button @click="editableTodo === todo.id ? saveEditedTodo(newEditableTodo, todo.id) : editTodo(todo)">{{ editableTodo === todo.id ? 'done' : 'edit' }}</button>
+        <button @click="removeTodo(todo.id)">remove</button>
+        </div>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
-import { getAll } from './api';
-import { mdiAccount } from '@mdi/js';
+import { getAll, addToDo, deleteToDo, editToDo } from './api';
 
 export default {
   name: 'App',
@@ -28,14 +40,50 @@ export default {
     return {
       items: [],
       rules: [(value) => !!value || 'Required.'],
-      task: '',
-      icons: { mdiAccount },
+      newTodo: '',
+      editableTodo: null,
+      newEditableTodo: '',
     };
   },
 
+
+  computed: {
+    itemsCount() {
+      return this.filteredItems.length;
+    },
+    filteredItems() {
+      return this.items.filter((item) => item.title.toLowerCase().includes(this.newTodo.toLowerCase()));
+    }
+  },
+
   methods: {
-    addTodo() {
-      this.itmes = [this.task, ...this.items];
+    async addTodo() {
+      const newTodo = this.newTodo && this.newTodo.trim();
+      if (!newTodo) {
+        return
+      }
+      const createdToDo = await addToDo(newTodo)
+      this.newTodo = ''
+      this.items = [createdToDo, ...this.items];
+    },
+
+    async removeTodo(todoId) {
+      await deleteToDo(todoId)
+      this.items = this.items.filter(({id})=> id !== todoId)
+    },
+  
+    editTodo({id, title}) {
+      this.editableTodo = id
+      this.newEditableTodo = title
+    },
+
+    async saveEditedTodo(title, id) {
+      if (!title) {
+        this.removeTodo(id)
+      }
+      const editedTodo = await editToDo(title, id);
+      this.items = this.items.filter((todo)=> todo.id !== id ? todo : editedTodo)
+      this.editableTodo =  null
     },
   },
 
@@ -44,3 +92,27 @@ export default {
   },
 };
 </script>
+<style>
+
+.input__container {
+  width: 60%;
+  margin: 0 auto;
+}
+
+.todos__wrapper {
+  width: 70%;
+  margin: 0 auto;
+}
+
+.todo__container {
+  display: flex;
+  justify-content: space-between;
+}
+
+.button__wrapper {
+  width: 100px;
+  display: flex;
+  justify-content: space-between;
+}
+
+</style>
--- todo-list/src/api/index.js
+++ todo-list/src/api/index.js
@@ -2,4 +2,35 @@ const API_URL = 'https://jsonplaceholder.typicode.com';
 export const getAll = () => {
   return fetch(`${API_URL}/todos`).then((response) => response.json());
 };
-// const getById = (id = `${API_URL}/todos/${id}`);
+
+export const addToDo = (task) => {
+  return fetch(`${API_URL}/todos`, {
+    method: 'POST',
+    body: JSON.stringify({
+      title: task,
+      completed: false,
+      userId: 1,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then((response) => response.json());
+};
+
+export const deleteToDo = (taskId) => {
+  return fetch(`${API_URL}/todos/${taskId}`, {
+    method: 'DELETE',
+  });
+};
+
+export const editToDo = (newToDo, id) => {
+  return fetch(`${API_URL}/todos/${id}`, {
+    method: 'PATCH',
+    body: JSON.stringify({
+      title: newToDo,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then((response) => response.json());
+};
