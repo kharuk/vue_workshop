1420e63350234301756577ba1d8306b680316b82
kharuk <kharuk.nastya@gmail.com>
2020-11-10 18:14:30 -0500

    clear master

--- todo-list/src/api/index.js
+++
@@ -1,5 +0,0 @@
-const API_URL = 'https://jsonplaceholder.typicode.com';
-export const getAll = () => {
-  return fetch(`${API_URL}/todos`).then((response) => response.json());
-};
-// const getById = (id = `${API_URL}/todos/${id}`);
